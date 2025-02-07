f83ba98b9cd77ebebebb7dd6fab7f209e7f2adbc
qichunren <whyruby@gmail.com>
2025-02-07 17:30:10 +0800

    Install tailwindCSS v4: https://tailwindcss.com/docs/installation/using-vite

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
