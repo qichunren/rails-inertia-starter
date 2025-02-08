f863440f38055640e4b712734c7f906e720b692f
qichunren <whyruby@gmail.com>
2025-02-08 20:03:38 +0800

    Fixed user logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  inertia_share do
+    if authenticated?
+      {
+        user: Current.user
+      }
+    end
+  end
 end
